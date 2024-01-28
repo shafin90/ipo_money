05f02ef592d0e45a4ac8d27d6d1c3a8946fc1c78
shafin90 <mashrafiahnam1@gmail.com>
2024-01-28 04:16:56 -0800

    careers section changes

--- src/app/components/Careers/Careers.jsx
+++ src/app/components/Careers/Careers.jsx
@@ -1,46 +1,34 @@
+'use client'
+import { LuUploadCloud } from "react-icons/lu";
 
 const Careers = () => {
+  const handleFileUpload = (e) => {
+    const selectedFile = e.target.files[0];
+    // Handle the selected file as needed
+    console.log("Selected file:", selectedFile);
+  };
+
   return (
+    <div id="careers" className="w-11/12 mx-auto mb-96">
+      <h1 className="text-center font-extrabold text-5xl text-slate-600 mb-16">
+        Careers
+      </h1>
 
-    <div id="careers" className='w-11/12 mx-auto mb-96'>
-      <h1 className='text-center font-extrabold text-5xl text-slate-600 mb-16'>Careers</h1>
-
-      <div className="hero bg-gray-50 hover:bg-gray-100 transition-all text-slate-600 rounded-2xl py-20">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-
-          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-gray-50 transition-all text-slate-600">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-lg font-bold">Your Name</span>
-                </label>
-                <input type="text" placeholder="Your Name" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-lg font-bold">Your Email Address</span>
-                </label>
-                <input type="email" placeholder="your email address" className="input input-bordered" required />
-
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-lg font-bold">Write your message</span>
-                </label>
-                <label className="form-control">
-
-                  <textarea className="textarea textarea-bordered h-24" placeholder="write your message"></textarea>
-
-                </label>
-
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn bg-[#bfdbfe] text-gray-800 hover:text-white border-0">Submit</button>
-              </div>
-            </form>
-          </div>
-        </div>
+      <div className=" relative hero bg-gray-50 transition-all text-slate-600 rounded-2xl py-24">
+        <label htmlFor="fileInput" className="cursor-pointer border-4 border-dashed p-10 rounded-2xl">
+          <LuUploadCloud className=" text-6xl transition-all text-gray-600" />
+        </label>
+        
+        <input
+          type="file"
+          id="fileInput"
+          accept=".pdf, .doc, .docx"
+          className="hidden"
+          onChange={handleFileUpload}
+        />
+      <h1 className=" text-center absolute bottom-10 text-xl font-semibold">Upload your Resume/CV </h1>
       </div>
+      
     </div>
   );
 };
